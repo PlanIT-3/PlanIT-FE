03bcc034557c5cda9f0a137b108571952adf05bf
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-22 16:18:31 +0900

    feat: add nav bar

---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="fixed bottom-0 left-1/2 -translate-x-1/2 w-[393px] bg-white border-t border-gray-200 flex justify-between px-4 py-2 z-20"
+  >
+    <div class="flex flex-col items-center text-xs text-gray-500">리포트</div>
+    <div class="flex flex-col items-center text-xs text-gray-500">추천</div>
+    <div class="flex flex-col items-center text-xs text-gray-500">홈</div>
+    <div class="flex flex-col items-center text-xs text-gray-500">목표</div>
+    <div class="flex flex-col items-center text-xs text-gray-500">리워드</div>
+  </div>
+</template>
+
+<script setup>
+// 별도 로직 없음
+</script>
