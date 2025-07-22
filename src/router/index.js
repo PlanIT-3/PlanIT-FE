fa39c2c54dca426d4f534c7cc0b85cc014e89116
greensnapback0229 <smdmim@gmail.com>
2025-07-22 15:15:28 +0900

    feat: 설문조사 페이지 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import InvestmentSurveyView from "../views/onBoarding/InvestmentSurveyView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/onBoarding/investment-survey",
+      name: "InvestmentSurvey",
+      component: InvestmentSurveyView,
     },
   ],
 });
