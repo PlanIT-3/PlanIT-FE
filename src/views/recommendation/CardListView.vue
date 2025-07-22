25802c87d14d75af3e0f5dda1b100e0a59cafa1e
bronze_yun_4521 <ehddbs452100@naver.com>
2025-07-22 16:19:37 +0900

    feat: add card list view

---
+++ src/views/recommendation/CardListView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col gap-4 w-[366px] mx-auto py-8">
+    <Card title="TIGER 미국나스닥100" ratio="추천 비중: 40%" yield="수익률 12.5%" risk="중간" riskVariant="one" />
+    <Card title="KODEX 200" ratio="추천 비중: 25%" yield="수익률 8.2%" risk="낮음" riskVariant="two" />
+    <Card title="ARIRANG 미국S&P500" ratio="추천 비중: 20%" yield="수익률 11.8%" risk="중간" riskVariant="one" />
+    <Card title="TIGER 차이나전기차" ratio="추천 비중: 15%" yield="수익률 15.3%" risk="높음" riskVariant="three" />
+  </div>
+</template>
+
+<script setup>
+import Card from "@/components/recommendation/Card.vue";
+</script>
