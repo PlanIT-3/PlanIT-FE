c0cea066e105dd1da8252b9ab9f9d3a39347d12a
seungwon-yu <y123587979@gmail.com>
2025-07-22 17:51:21 +0900

    feat: main 페이지 생성

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,8 @@ import InvestmentSurveyResult from "../views/onBoarding/InvestmentSurveyResult.v
 import BankSelectView from "../views/onBoarding/BankSelectView.vue";
 import BankLoginView from "../views/onBoarding/BankLoginView.vue";
 import BankLinkingSuccessView from "../views/onBoarding/BankLinkingSuccessView.vue";
-import BankSelectView from "../views/onBoarding/bankSelectView.vue";
-import BankLoginView from "../views/onBoarding/bankLoginView.vue";
 import RecommendView from "../views/recommendation/RecommendView.vue";
+import MainView from "../views/MainView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -46,6 +45,11 @@ const router = createRouter({
       path: "/recommend",
       name: "recommend",
       component: RecommendView,
+    },
+    {
+      path: "/main",
+      name: "main",
+      component: MainView,
     },
   ],
 });
