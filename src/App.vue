4357d00c9486831ac40f61bc54431694db4789bc
seungwon-yu <y123587979@gmail.com>
2025-07-23 23:33:10 +0900

    chore: App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <!-- 루트 애플리케이션 컴포넌트 -->
 <template>
   <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-    <div id="app" class="w-[393px] h-[852px] bg-white shadow-md overflow-hidden relative">
-      <!-- TODO: 앱 레이아웃 구현 -->
-      <component :is="layoutHeader" />
-      <RouterView></RouterView>
-      <NavBar></NavBar>
+    <div id="app" class="w-[393px] h-[852px] bg-white shadow-md overflow-y-auto overflow-x-visible relative flex flex-col">
+      <div class="flex-shrink-0">
+        <component :is="layoutHeader" />
+      </div>
+      <div class="flex-1 min-h-0">
+        <RouterView></RouterView>
+      </div>
+      <div class="flex-shrink-0">
+        <NavBar></NavBar>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-// TODO: 앱 로직 구현
 import { RouterView } from "vue-router";
 import { computed } from "vue";
 import { useRoute } from "vue-router";
+import { onMounted } from "vue";
 import MainHeader from "./components/common/MainHeader.vue";
 import NavBar from "./components/common/NavBar.vue";
 import OnboardingHeader from "@/components/common/OnboardingHeader.vue";
@@ -26,6 +31,17 @@ const layoutHeader = computed(() => {
   if (layout === "onboardHeader") return OnboardingHeader;
   if (layout === "noHeader") return null;
   return MainHeader;
+});
+
+onMounted(() => {
+  //뷰포트
+  const metaViewport = document.querySelector('meta[name="viewport"]');
+  if (metaViewport) {
+    metaViewport.setAttribute(
+      "content",
+      "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+    );
+  }
 });
 </script>
 
