afe3046df0cdd282d67ee1e9c84e3471b95b8479
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-07-25 10:48:07 +0900

    chore: layout 폴더 통합

---
+++ src/components/layout/MainLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MainHeader from "../common/MainHeader.vue";
+import NavBar from "../common/NavBar.vue";
+</script>
+
+<template>
+  <MainHeader></MainHeader>
+  <slot></slot>
+  <NavBar></NavBar>
+</template>
