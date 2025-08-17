72b20009e15e732d4d29722be3eeb89d19665cbb
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-08-17 21:40:22 +0900

    refactor: 목표 카드 layout 글자 수정

--- src/components/goal/GoalCard.vue
+++ src/components/goal/GoalCard.vue
@@ -84,8 +84,8 @@ const statusTextColorComputed = computed(() => {
       <BarChart :data="barChartData" :labelValues="props.labelValues" />
       <!-- 바 차트 밑 텍스트 -->
       <div class="flex justify-between text-xs font-medium mt-1">
-        <span>{{ props.totalAmount }}</span>
-        <span>{{ props.targetAmount }}</span>
+        <span>현재 금액: {{ props.totalAmount.toLocaleString() }}원</span>
+        <span>목표 금액: {{ props.targetAmount.toLocaleString() }}원</span>
       </div>
     </div>
   </div>
