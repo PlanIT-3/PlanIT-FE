30caeb798f0b318d10a44b999980845695976398
seungwon-yu <y123587979@gmail.com>
2025-08-19 17:54:52 +0900

    fix:object에 updateGoal추가

--- src/api/objectApi.js
+++ src/api/objectApi.js
@@ -13,6 +13,13 @@ export default {
     return api.put(`${BASE_URL}/${goalId}`, goalData);
   },
 
+  //목표 수정
+  async updateGoal(goalId, goalData) {
+    const response = await api.put(`${BASE_URL}/${goalId}`, goalData);
+    console.log("UPDATE GOAL RESPONSE", response.data);
+    return response;
+  },
+
   //목표 리스트
   async getGoalList() {
     const { data } = await api.get(`${BASE_URL}`);
